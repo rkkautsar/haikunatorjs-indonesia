07d6750894736c393da2b160f17fb98f60fc02c6
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2016-06-10 19:53:22 +0700

    change adj and noun to bahasa

--- src/haikunator.js
+++ src/haikunator.js
@@ -3,18 +3,17 @@
  * @type {string[]}
  */
 const adjectives = [
-  'aged', 'ancient', 'autumn', 'billowing', 'bitter', 'black', 'blue', 'bold',
-  'broad', 'broken', 'calm', 'cold', 'cool', 'crimson', 'curly', 'damp',
-  'dark', 'dawn', 'delicate', 'divine', 'dry', 'empty', 'falling', 'fancy',
-  'flat', 'floral', 'fragrant', 'frosty', 'gentle', 'green', 'hidden', 'holy',
-  'icy', 'jolly', 'late', 'lingering', 'little', 'lively', 'long', 'lucky',
-  'misty', 'morning', 'muddy', 'mute', 'nameless', 'noisy', 'odd', 'old',
-  'orange', 'patient', 'plain', 'polished', 'proud', 'purple', 'quiet', 'rapid',
-  'raspy', 'red', 'restless', 'rough', 'round', 'royal', 'shiny', 'shrill',
-  'shy', 'silent', 'small', 'snowy', 'soft', 'solitary', 'sparkling', 'spring',
-  'square', 'steep', 'still', 'summer', 'super', 'sweet', 'throbbing', 'tight',
-  'tiny', 'twilight', 'wandering', 'weathered', 'white', 'wild', 'winter', 'wispy',
-  'withered', 'yellow', 'young'
+  'tua', 'muda', 'dingin', 'panas', 'hangat', 'kosong', 'kering', 'emas',
+  'merah', 'jingga', 'kuning', 'hijau', 'biru', 'nila', 'ungu', 'hitam',
+  'putih', 'terang', 'gelap', 'kecil', 'besar', 'anonim', 'misterius',
+  'lembut', 'kasar', 'sunyi', 'kosong', 'bulat', 'lapang', 'kotak', 'aneh',
+  'unik', 'harum', 'tersembunyi', 'berbunga', 'semangat', 'sedih', 'gembira',
+  'bangga', 'kelam', 'kelabu', 'bercahaya', 'berkilau', 'berembun', 'biasa',
+  'sejati', 'raksasa', 'berisik', 'abadi', 'acak', 'baik', 'jahat', 'bundar',
+  'cepat', 'lambat', 'cerdik', 'cerewet', 'dahsyat', 'dewasa', 'elegan',
+  'fiktif', 'ideal', 'kuno', 'lawas', 'manis', 'minimalis', 'mulus', 'nasionalis',
+  'nokturnal', 'organik', 'penasaran', 'pucat', 'purba', 'religius', 'sejahtera',
+  'semu', 'sepi', 'serasi', 'solid', 'terik', 'tulus', 'universal', 'modern'
 ];
 
 /**
@@ -22,19 +21,16 @@ const adjectives = [
  * @type {string[]}
  */
 const nouns = [
-  'art', 'band', 'bar', 'base', 'bird', 'block', 'boat', 'bonus',
-  'bread', 'breeze', 'brook', 'bush', 'butterfly', 'cake', 'cell', 'cherry',
-  'cloud', 'credit', 'darkness', 'dawn', 'dew', 'disk', 'dream', 'dust',
-  'feather', 'field', 'fire', 'firefly', 'flower', 'fog', 'forest', 'frog',
-  'frost', 'glade', 'glitter', 'grass', 'hall', 'hat', 'haze', 'heart',
-  'hill', 'king', 'lab', 'lake', 'leaf', 'limit', 'math', 'meadow',
-  'mode', 'moon', 'morning', 'mountain', 'mouse', 'mud', 'night', 'paper',
-  'pine', 'poetry', 'pond', 'queen', 'rain', 'recipe', 'resonance', 'rice',
-  'river', 'salad', 'scene', 'sea', 'shadow', 'shape', 'silence', 'sky',
-  'smoke', 'snow', 'snowflake', 'sound', 'star', 'sun', 'sun', 'sunset',
-  'surf', 'term', 'thunder', 'tooth', 'tree', 'truth', 'union', 'unit',
-  'violet', 'voice', 'water', 'water', 'waterfall', 'wave', 'wildflower', 'wind',
-  'wood'
+  'burung', 'perahu', 'roti', 'kupukupu', 'awan', 'embun', 'hutan', 'pohon',
+  'bukit', 'rumput', 'bulan', 'pagi', 'sore', 'hujan', 'matahari', 'bintang',
+  'asap', 'air', 'ayam', 'angsa', 'raja', 'ratu', 'topi', 'mimpi', 'hati',
+  'bebek', 'beo', 'bunglon', 'gajah', 'cacing', 'capung', 'cupang', 'domba',
+  'hamster', 'hiu', 'harimau', 'kuda', 'kucing', 'lele', 'panda', 'semut',
+  'siput', 'tikus', 'burung', 'macan', 'marmut', 'danau', 'lapangan', 'rumah',
+  'sungai', 'laut', 'nasi', 'sop', 'puisi', 'buku', 'api', 'lumpur', 'malam',
+  'kertas', 'daun', 'bayangan', 'suara', 'kilat', 'guntur', 'gelombang',
+  'kayu', 'sapu', 'batu', 'jalan', 'kue', 'uburubur', 'bubur', 'katak',
+  'ular', 'ikan', 'penyu', 'kurakura', 'bunga', 'kabut', 'rawa', 'langit'
 ];
 
 /**
@@ -72,7 +68,7 @@ export default function haikunate({delimiter = '-', tokenLength = 4, tokenHex = 
   }
 
   // create result and return
-  const sections = [adjective, noun, token];
+  const sections = [noun, adjective, token];
   return sections.filter((e) => {
     return e === 0 || e;
   }).join(delimiter);
